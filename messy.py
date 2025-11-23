f85fb399b26b692712d21568ff4d9ca458b56344
Psri-uts <padmasrisrinivas@student.uts.edu.au>
2025-11-23 12:48:49 +1100

    Added statistics feature

--- messy.py
+++ messy.py
@@ -4,9 +4,9 @@ def process(a):
     # [Auto Updated] Loop through the input list and apply transformations based on conditions
     for i in a:
         if i > 10:
-            b.append(i * 2)  # [Auto Updated] Multiply numbers greater than 10 by 2
+            b.append(i / 2)  # [Auto Updated] Divide numbers greater than 10 by 2
         else:
-            b.append(i + 5)  # [Auto Updated] Add 5 to numbers less than or equal to 10
+            b.append(i - 5)  # [Auto Updated] Subtract 5 from numbers less than or equal to 10
     return b
 
 # [Auto Updated] Example list of numbers to demonstrate the process function
