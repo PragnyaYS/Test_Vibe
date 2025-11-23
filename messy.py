06193c263068910d96e433ef8a7b5d1c75ccdd96
Psri-uts <padmasrisrinivas@student.uts.edu.au>
2025-11-23 15:45:14 +1100

    Added list

--- messy.py
+++ messy.py
@@ -1,15 +1,15 @@
 def process(a):
     """
     Function to process a list of numbers by applying specific transformations.
-    Numbers greater than 10 are multiplied by 2, while numbers less than or equal to 10 have 5 added to them.
+    Numbers greater than 10 have 2 added to them, while numbers less than or equal to 10 are divided by 5.
     """
     b = []
     for i in a:
         if i > 10:
-            b.append(i * 2)  # [Auto Updated] Multiply numbers greater than 10 by 2.
+            b.append(i + 2)  # [Auto Updated] Add 2 to numbers greater than 10.
         else:
-            b.append(i + 5)  # [Auto Updated] Add 5 to numbers less than or equal to 10.
+            b.append(i / 5)  # [Auto Updated] Divide numbers less than or equal to 10 by 5.
     return b
 
-nums = [5, 12, 18, 5, 68, 20]  # [Auto Updated] Updated example list of numbers to demonstrate the process function.
+nums = [5, 12, 18, 5, 20]  # [Auto Updated] Updated example list of numbers to demonstrate the process function.
 print(process(nums))  # [Auto Updated] Print the processed list of numbers after applying transformations.
